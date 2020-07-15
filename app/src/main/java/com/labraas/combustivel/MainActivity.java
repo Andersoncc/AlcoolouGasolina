409892315beaf15ae0fe5508e4aadb5b873302f2
Anderson <andercosta87@gmail.com>
2020-07-14 22:26:28 -0300

    Versão final a que será ppublicada

--- app/src/main/java/com/labraas/combustivel/MainActivity.java
+++ app/src/main/java/com/labraas/combustivel/MainActivity.java
@@ -2,11 +2,17 @@ package com.labraas.combustivel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -20,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private InterstitialAd mInterstitialAd;
     private TextInputEditText editPrecoAlcool, editPrecoGasolina;
     private TextView textResultado;
-    //private Button mBotao = (Button) findViewById(R.id.btnCalcular);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,48 +34,48 @@ public class MainActivity extends AppCompatActivity {
 
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId("ca-app-pub-9584107572536485/2112277072");
-        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        AdRequest adRequesInter = new AdRequest.Builder().build();
+        mInterstitialAd.loadAd(adRequesInter);
 
         MobileAds.initialize(this, "ca-app-pub-9584107572536485~3440002314");
-        mInterstitialAd.loadAd(new AdRequest.Builder().build());
         adview = (AdView) findViewById(R.id.adViewBan);
         AdRequest adRequest =  new AdRequest.Builder().build();
         adview.loadAd(adRequest);
-
 
         editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
         editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
         textResultado = findViewById(R.id.textResultado);
 
+        SimpleMaskFormatter smf = new SimpleMaskFormatter("N.NN");
+        MaskTextWatcher mtwA = new MaskTextWatcher(editPrecoAlcool, smf);
+        MaskTextWatcher mtwG = new MaskTextWatcher(editPrecoGasolina, smf);
+        editPrecoAlcool.addTextChangedListener(mtwA);
+        editPrecoGasolina.addTextChangedListener(mtwG);
+
 
     }
 
-
-    /*mBotao.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (mInterstitialAd.isLoaded()) {
-                mInterstitialAd.show();
-            } else {
-                Log.d("TAG", "The interstitial wasn't loaded yet.");
-            }
-        }
-    });*/
-
-
-
-
-
     public void calcularPreco( View view){
+
+
+
         String precoAlcool = editPrecoAlcool.getText().toString();
         String precoGasolina = editPrecoGasolina.getText().toString();
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
         boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
         if(camposValidados){
-           Double valorAlcool = Double.parseDouble(precoAlcool);
+
+            Double valorAlcool = Double.parseDouble(precoAlcool);
             Double valorGasolina = Double.parseDouble(precoGasolina);
             if(valorAlcool / valorGasolina >=0.7){
+                textResultado.setTextColor(Color.parseColor("#ff0000"));
                 textResultado.setText("É melhor utilizar Gasolina");
             }else {
+                textResultado.setTextColor(Color.parseColor("#2c3d91"));
                 textResultado.setText("É melhor utilizar Álcool");
             }
 
@@ -79,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
         }else{
             textResultado.setText("preencha todos os campos!");
         }
+
+        fecharTeclado();
 
 
     }
@@ -92,6 +98,15 @@ public class MainActivity extends AppCompatActivity {
              camposValidados = false;
          }
              return camposValidados;
+    }
+
+    private void fecharTeclado(){
+        View view = this.getCurrentFocus();
+        if(view != null){
+            InputMethodManager fech = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            fech.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        }
     }
 
 
